44f747e789ccf83e0e9cb5d2a0510ddd159dafa6
Pavel <pashakka@gmail.com>
2015-12-17 17:14:32 +0200

    MyView changes

--- carpad/src/main/java/ua/od/pashakka/carpad/MyView.java
+++ carpad/src/main/java/ua/od/pashakka/carpad/MyView.java
@@ -1,8 +1,47 @@
 package ua.od.pashakka.carpad;
 
-/**
- * Created by pasha on 017.17.12.2015.
- */
-public class MyView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
 
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int measuredHeight = measureHeight(heightMeasureSpec);
+        int measuredWidth = measureWidth(widthMeasureSpec);
+
+        setMeasuredDimension(measuredHeight, measuredWidth);
+    }
+
+    private int measureHeight(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        return specSize;
+    }
+
+    private int measureWidth(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        return specSize;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
 }
