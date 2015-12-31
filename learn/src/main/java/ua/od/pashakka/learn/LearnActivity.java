a726828221bb7b2573b735c024aafc376ddb32b8
Pavel <pashakka@gmail.com>
2015-12-31 10:47:29 +0200

    Перед НГ

---
+++ learn/src/main/java/ua/od/pashakka/learn/LearnActivity.java
@@ -0,0 +1,48 @@
+package ua.od.pashakka.learn;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class LearnActivity extends Activity {
+
+    ArrayList<Product> products = new ArrayList<Product>();
+    BoxAdapter boxAdapter;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn);
+
+        // создаем адаптер
+        fillData();
+        boxAdapter = new BoxAdapter(this, products);
+
+        // настраиваем список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+        lvMain.setAdapter(boxAdapter);
+    }
+
+    // генерируем данные для адаптера
+    void fillData() {
+        for (int i = 1; i <= 20; i++) {
+            products.add(new Product("Product " + i, i * 1000,
+                    R.drawable.add, false));
+        }
+    }
+
+    // выводим информацию о корзине
+    public void showResult(View v) {
+        String result = "Товары в корзине:";
+        for (Product p : boxAdapter.getBox()) {
+            if (p.box)
+                result += "\n" + p.name;
+        }
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+    }
+}
